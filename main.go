fade6fb6594191396ac0f81251e3e3bf837f9c43
ncls-brn <nicolas.j.baran@gmail.com>
2022-12-01 16:16:24 +0100

    mep functions

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import("")
+
+func problemPuller(fileName string)([]problem,error){
+
+}
+
+func main(){
+
+}
+
+func parseProblem(lines [][]string) []problem{
+
+}
+
+type problem struct {
+	q string
+	a string
+}
